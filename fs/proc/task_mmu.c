45639d72b29980e101216bdb8d1c1f8a2efc38d1
ZyCromerZ <neetroid97@gmail.com>
2021-09-28 12:36:46 +0700

    fs: proc: tash_mmu: add new module name use_sultan_pid * to force enable sultan_pid and sultan_pid_shrink
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -27,6 +27,16 @@
 #include <misc/lyb_taskmmu.h>
 #include "internal.h"
 
+
+bool sultan_pid = false;
+module_param(sultan_pid, bool, 0644);
+
+bool sultan_pid_shrink = false;
+module_param(sultan_pid_shrink, bool, 0644);
+
+bool use_sultan_pid = false;
+module_param(use_sultan_pid, bool, 0644);
+
 #define SEQ_PUT_DEC(str, val) \
 		seq_put_decimal_ull_width(m, str, (val) << (PAGE_SHIFT-10), 8)
 void task_mem(struct seq_file *m, struct mm_struct *mm)
@@ -529,7 +539,12 @@ static int show_vma_header_prefix(struct seq_file *m, unsigned long start,
 	/* Supports printing up to 40 bits per virtual address */
 	BUILD_BUG_ON(CONFIG_ARM64_VA_BITS > 40);
 
-	if (lyb_sultan_pid_shrink)
+	if (use_sultan_pid)
+	{
+		sultan_pid_shrink = true;
+	}
+
+	if (sultan_pid_shrink)
 	{
 		/* 
 		 * shrinks the PID map output to be as small as
@@ -780,7 +795,12 @@ static const struct seq_operations proc_tid_maps_op = {
 
 static int pid_maps_open(struct inode *inode, struct file *file)
 {
-	if (lyb_sultan_pid)
+
+	if (use_sultan_pid)
+	{
+		sultan_pid = true;
+	}
+	if (sultan_pid)
 		return do_maps_open(inode, file, &proc_pid_maps_op_sultanpid);
 	else return do_maps_open(inode, file, &proc_pid_maps_op);
 }
