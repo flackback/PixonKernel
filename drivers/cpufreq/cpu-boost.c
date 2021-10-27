98d8e826ac5256419dbeed46294e7cb34cd21fac
Anderson Fagundes <deloren@live.com>
2021-10-27 09:46:30 -0300

    Revert "cpu-boost: Update functions for newer Dynamic Schedtune Boost changes"
    This reverts commit 856180bbc37aac426a0027e81ab0cec09220f1b0.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -47,8 +47,6 @@ static bool sched_boost_active;
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 static int dynamic_stune_boost;
 module_param(dynamic_stune_boost, uint, 0644);
-static bool stune_boost_active;
-static int boost_slot;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 static struct delayed_work input_boost_rem;
@@ -185,10 +183,7 @@ static void do_input_boost_rem(struct work_struct *work)
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Reset dynamic stune boost value to the default value */
-	if (stune_boost_active) {
-		reset_stune_boost("top-app", boost_slot);
-		stune_boost_active = false;
-	}
+	reset_stune_boost("top-app");
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	/* Update policies for all online CPUs */
@@ -213,13 +208,6 @@ static void do_input_boost(struct work_struct *work)
 		sched_boost_active = false;
 	}
 
-
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	if (stune_boost_active) {
-		reset_stune_boost("top-app", boost_slot);
-		stune_boost_active = false;
-	}
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 	/* Set the input_boost_min for all CPUs in the system */
 	pr_debug("Setting input boost min for all CPUs\n");
 	for_each_possible_cpu(i) {
@@ -245,13 +233,7 @@ static void do_input_boost(struct work_struct *work)
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Set dynamic stune boost value */
-<<<<<<< HEAD
 	do_stune_boost("top-app", dynamic_stune_boost);
-=======
-	ret = do_stune_boost("top-app", dynamic_stune_boost, &boost_slot);
-	if (!ret)
-		stune_boost_active = true;
->>>>>>> 1b13aca48a87f... cpu-boost: Update functions for newer Dynamic Schedtune Boost changes
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	schedule_delayed_work(&input_boost_rem, msecs_to_jiffies(input_boost_ms));
@@ -344,7 +326,7 @@ static void cpuboost_input_disconnect(struct input_handle *handle)
 {
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Reset dynamic stune boost value to the default value */
-	reset_stune_boost("top-app", boost_slot);
+	reset_stune_boost("top-app");
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	input_close_device(handle);
